bce4f2960c4176b271634c415275aa998fc2d692
p22.marques <htadeu@hotmail.com>
2021-10-23 20:58:02 -0300

    lesson03 04 - Sistema de acesso.

---
+++ src/games/access_system.py
@@ -0,0 +1,19 @@
+def main():
+  print('*'*50)
+  print('*{:^48}*'.format(' Access System '))
+  print('*'*50, end="\n")
+  print()
+
+  usuario = input('Informe o usuário do sistema!\n').capitalize()
+
+  if(usuario == "Flávio"):
+    print("Seja bem-vindo {}".format(usuario))
+  elif(usuario == "Douglas"):
+      print("Seja bem-vindo {}".format(usuario))
+  elif(usuario == "Nico"):
+    print("Seja bem-vindo {}".format(usuario))
+  else:
+    print("Usuário ({}) não identificado!".format(usuario.upper()))
+  
+  print('\n{:*^50}'.format(' fim do programa ', end="\n"))
+main()
