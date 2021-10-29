1cf3ae0927facbfdd1af74cdbd1aba8174556903
p22.marques <htadeu@hotmail.com>
2021-10-28 21:00:38 -0300

    lesson08 08 - Mão na massa: Modularizando o jogo

--- src/games/gallows_game.py
+++ src/games/gallows_game.py
@@ -1,7 +1,11 @@
-print("{}".format("*"*50))
-print('{:*^50}'.format(' >>>  Welcome To The Gallows Game!  <<< '))
-print("{}".format("*"*50))
+def play():
+  print("{}".format("*"*50))
+  print('{:*^50}'.format(' >>>  Welcome To The Gallows Game!  <<< '))
+  print("{}".format("*"*50))
 
 
 
-print("{:*^50}".format('<<<  Game Over  >>>'))
+  print("{:*^50}".format('<<<  Game Over  >>>'))
+
+if (__name__ == "__main__"):
+  play()
