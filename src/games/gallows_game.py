5a938c6456b9c395a4108d9461e5227dc3926f26
p22.marques <htadeu@hotmail.com>
2021-11-03 07:27:39 -0300

    lesson05 11 - Mãos na massa: encerrando o jogo

--- src/games/gallows_game.py
+++ src/games/gallows_game.py
@@ -1,48 +1,48 @@
 def play():
-  print("{}".format("*"*50))
-  print('{:*^50}'.format(' >>>  Welcome To The Gallows Game!  <<< '))
-  print("{}".format("*"*50))
+	print("{}".format("*"*50))
+	print('{:*^50}'.format(' >>>  Welcome To The Gallows Game!  <<< '))
+	print("{}".format("*"*50))
 
-  secret_word = "banana".upper()
-  letter_right = ['_' for letter in secret_word]
+	secret_word = "banana".upper()
+	letter_right = ['_' for letter in secret_word]
 
-  right = False
-  gallows = False
-  missed = len(secret_word)
+	right = False
+	gallows = False
+	missed = len(secret_word)
 
-  print(letter_right)
-  
-  while(not right and not gallows):
-    kick = input("Type a letter: ")
-    kick = kick.strip().upper()
+	print(letter_right)
+	
+	while(not right and not gallows):
+		kick = input("Type a letter: ")
+		kick = kick.strip().upper()
 
-    if(kick in secret_word):
-      index = 0
-      for letter in secret_word:
-        if(kick == letter):
-          letter_right[index] = letter
-        index += 1
+		if(kick in secret_word):
+			index = 0
+			for letter in secret_word:
+				if(kick == letter):
+					letter_right[index] = letter
+				index += 1
 
-      letter_left = letter_right.count('_')  # Hamilton
-      print('Still left to hit {} letters.'.format(letter_left))  # Hamilton
+			letter_left = letter_right.count('_')  # Hamilton
+			print('Still left to hit {} letters.'.format(letter_left))  # Hamilton
 
-      if(letter_left != 0):  # Hamilton
-        print("Playing...")
-    else:
-      missed -= 1 # Hamilton
-      print("{} attempts remain.".format(missed))  # Hamilton
+			if(letter_left != 0):  # Hamilton
+				print("Playing...")
+		else:
+			missed -= 1 # Hamilton
+			print("{} attempts remain.".format(missed))  # Hamilton
 
-    gallows = missed == 0
-    right = '_' not in letter_right
+		gallows = missed == 0
+		right = '_' not in letter_right
 
-    print(letter_right)
+		print(letter_right)
 
-  if(right):
-    print("YOU WIN :) CONGRATULATION!")
-  else:
-    print("YOU LOSE :( SORRY!")
+	if(right):
+		print("YOU WIN :) CONGRATULATION!")
+	else:
+		print("YOU LOSE :( SORRY!")
 
-  print("{:*^50}".format('<<<  Game Over  >>>'))
+	print("{:*^50}".format('<<<  Game Over  >>>'))
 
 if (__name__ == "__main__"):
-  play()
+	play()
