ce4367df42ee2d43dcdf0fb1f90bebe27e914b1a
p22.marques <htadeu@hotmail.com>
2021-11-10 20:13:10 -0300

    lesson03 01 - Usando métodos

--- HbFintech/src/conta.py
+++ HbFintech/src/conta.py
@@ -5,3 +5,14 @@ class Conta:
         self.titular = titular
         self.saldo = saldo
         self.limite = limite
+        
+    def extrato(self):
+        print("Olá {}, seu saldo é R${}".format(
+            self.titular, 
+            self.saldo))
+        
+    def deposita(self, valor):
+        self.saldo += valor
+        
+    def saca(self, valor):
+        self.saldo -= valor
