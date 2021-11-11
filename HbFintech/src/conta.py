9f21f48ca61c033ff5d636f0f61c8ff108045300
p22.marques <htadeu@hotmail.com>
2021-11-11 15:34:11 -0300

    lesson04 01 - Atributos privados

--- HbFintech/src/conta.py
+++ HbFintech/src/conta.py
@@ -1,10 +1,10 @@
 class Conta:
     def __init__(self, numero, titular, saldo, limite = 1000.0):
         print("Construindo objeto...{}".format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
         
     def extrato(self):
         print("Olá {}, seu saldo é R${}".format(
