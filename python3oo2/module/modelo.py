d237b5e8ab31f2b134a9be9adf6f656082b71f53
p22.marques <htadeu@hotmail.com>
2021-11-30 16:26:55 -0300

    ooalesson03 02 - Polimorfismo

--- python3oo2/module/modelo.py
+++ python3oo2/module/modelo.py
@@ -3,14 +3,14 @@ class Programa:
     def __init__(self, nome, ano):
         self._nome = nome.title()
         self.ano = ano
-        self._like = 0
+        self._likes = 0
     
     @property
-    def like(self):
-        return self._like
+    def likes(self):
+        return self._likes
     
     def dar_likes(self):
-        self._like += 1
+        self._likes += 1
         
     @property
     def nome(self):
@@ -27,15 +27,16 @@ class Filme(Programa):
         self.duracao = duracao
         
 """ Here Serie class """        
-class Serie:
+class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
-
+""" New objects """
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
 
+""" Invoke dar_likes method """
 vingadores.dar_likes()
 vingadores.dar_likes()
 vingadores.dar_likes()
@@ -43,6 +44,11 @@ vingadores.dar_likes()
 atlanta.dar_likes()
 atlanta.dar_likes()
 
-print(f'Nome: {vingadores.nome} - Likes: {vingadores.likes}')
-print(f'Nome: {atlanta.nome} - Likes: {atlanta.likes}')
-        
+""" Outputs """
+# print(f'Nome: {vingadores.nome} - Likes: {vingadores.likes}')
+# print(f'Nome: {atlanta.nome} - Likes: {atlanta.likes}')
+filmes_e_series = [vingadores, atlanta]
+
+for programa in filmes_e_series:
+    detalhes = programa.duracao if hasattr(programa, 'duracao') else programa.temporadas
+    print(f'{programa.nome} - {detalhes} D - {programa.likes}')
