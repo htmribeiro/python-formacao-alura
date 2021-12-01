9b19822364c8d0b56365dfc22241b65767e5077f
p22.marques <htadeu@hotmail.com>
2021-12-01 14:59:58 -0300

    ooalesson03 04 - Reduzindo ifs

--- python3oo2/module/modelo.py
+++ python3oo2/module/modelo.py
@@ -19,6 +19,9 @@ class Programa:
     @nome.setter
     def nome(self, nome):
         self._nome = nome
+        
+    def imprime(self):
+        print(f'{self._nome} - {self.ano} - {self._likes} Likes')
 
 """ Here Filme class """
 class Filme(Programa):
@@ -26,11 +29,17 @@ class Filme(Programa):
         super().__init__(nome, ano)
         self.duracao = duracao
         
+    def imprime(self):
+        print(f'{self._nome} - {self.ano} - {self.duracao} min - {self._likes} Likes')
+        
 """ Here Serie class """        
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
+    
+    def imprime(self):
+        print(f'{self._nome} - {self.ano} - {self.temporadas} temporadas - {self._likes} Likes')
 
 """ New objects """
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
@@ -45,10 +54,9 @@ atlanta.dar_likes()
 atlanta.dar_likes()
 
 """ Outputs """
-# print(f'Nome: {vingadores.nome} - Likes: {vingadores.likes}')
-# print(f'Nome: {atlanta.nome} - Likes: {atlanta.likes}')
 filmes_e_series = [vingadores, atlanta]
 
 for programa in filmes_e_series:
-    detalhes = programa.duracao if hasattr(programa, 'duracao') else programa.temporadas
-    print(f'{programa.nome} - {detalhes} D - {programa.likes}')
+    programa.imprime()
+    # detalhes = programa.duracao if hasattr(programa, 'duracao') else programa.temporadas
+    # print(f'{programa.nome} - {detalhes} D - {programa.likes}')
