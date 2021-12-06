acf6d1f44af48704884acb64c64b62d66dabe159
p22.marques <htadeu@hotmail.com>
2021-12-06 10:28:34 -0300

    ooalesson06 02 - Mais de uma classe mãe

---
+++ python3oo2/module/funcionario.py
@@ -0,0 +1,36 @@
+class Funcionario:
+    def registra_horas(self, horas):
+        print('Horas registradas...')
+        
+    def mostrar_tarefas(self):
+        print('Fez muita coisa...')
+        
+class Caelum(Funcionario):
+    def mostrar_tarefas(self):
+        print('Fez muita coisa, Caelumer')
+        
+    def buscar_cursos_do_mes(self, mes=None):
+        print(f'Mostrando cursos- {mes}' if mes else 'Mostrar cursos desse mês')
+
+class Alura(Funcionario):
+    def mostrar_tarefas(self):
+        print('Fez muita coisa, Alurete!')
+        
+    def buscar_perguntas_sem_resposta(self):
+        print('Mostrando perguntas não respondidas do fórum')
+
+class Junior(Alura):
+    pass
+
+class Pleno(Alura, Caelum):
+    pass
+
+jose = Junior()
+jose.buscar_perguntas_sem_resposta()
+
+luan = Pleno()
+luan.buscar_perguntas_sem_resposta()
+luan.buscar_cursos_do_mes()
+
+luan.mostrar_tarefas()
+        
